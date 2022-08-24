8091afa7a7163bf0af2522332edc4aa69e98f3c8
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-08-24 12:55:06 +0200

    Searchbar Includes OK

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # p7_les_petits_plats
  Site de recettes de cuisine avec tri
+
+
+for each btn, add event on click event listener
+
+function (e) { e.currentTarget.dataset.id }
+
+html : <div class="recipe" data-id="{{recipe.id}}">
+
--- index.js
+++ index.js
@@ -1,87 +1,124 @@
-import {recipes} from './data/recipes.js';
+import { recipes } from './data/recipes.js'
 
-console.log(recipes);
+console.log(recipes)
 
+let recipeList = recipes
 
+let actualRecipe = []
 
-const loop = [1, 2 ,3, 4, 5, 6, 7, 8, 9, 10];
+let ustensilsList = []
+let ingredientsList = []
+let appliancesList = []
 
-loop.forEach(element => {
- const recipe = document.createElement('article');
- recipe.classList.add('recipe');
-    // handle Image Prototype
-const img = new Image()
-img.src = './assets/recipes_photos/limonade_coco.jpg'
-recipe.appendChild(img);
-img.classList.add('recipe__image');
+recipes.forEach((recipes) => {
+  ustensilsList.push(recipes.ustensils)
+    ingredientsList.push(recipes.ingredients)
+  appliancesList.push(recipes.appliance)
+})
 
-// handle  texts Container Prototype
-const textsContainer = document.createElement('div');
-textsContainer.classList.add('recipe__texts');
-recipe.appendChild(textsContainer);
+console.log(ustensilsList)
+console.log(ingredientsList)
+console.log(appliancesList)
 
+recipeList.forEach((el) => {
+  const recipe = document.createElement('article')
+  recipe.classList.add('recipe')
+  // handle Image Prototype
+  const img = new Image()
+  img.src = './assets/recipes_photos/limonade_coco.jpg'
+  recipe.appendChild(img)
+  img.classList.add('recipe__image')
 
-// handle  title and Ingredients + time and Instructions Container Prototype
-const titleIngredientsContainer = document.createElement('div');
-const timeAndInstructionsContainer = document.createElement('div');
-titleIngredientsContainer.classList.add('recipe__title-ingredients');
-timeAndInstructionsContainer.classList.add('recipe__time-instructions');
+  // handle  texts Container Prototype
+  const textsContainer = document.createElement('div')
+  textsContainer.classList.add('recipe__texts')
+  recipe.appendChild(textsContainer)
 
-textsContainer.appendChild(titleIngredientsContainer);
-textsContainer.appendChild(timeAndInstructionsContainer);
+  // handle  title and Ingredients + time and Instructions Container Prototype
+  const titleIngredientsContainer = document.createElement('div')
+  const timeAndInstructionsContainer = document.createElement('div')
+  titleIngredientsContainer.classList.add('recipe__title-ingredients')
+  timeAndInstructionsContainer.classList.add('recipe__time-instructions')
 
+  textsContainer.appendChild(titleIngredientsContainer)
+  textsContainer.appendChild(timeAndInstructionsContainer)
 
-// handle  title Prototype
-const title = document.createElement('h2');
-title.classList.add('recipe__title');
-title.textContent = 'Limonade coco';
+  // handle  title Prototype
+  const title = document.createElement('h2')
+  title.classList.add('recipe__title')
+  title.textContent = el.name
 
-titleIngredientsContainer.appendChild(title);
+  titleIngredientsContainer.appendChild(title)
 
-// handle  ingredients Prototype
-const ingredients = document.createElement('ul');
-ingredients.classList.add('recipe__ingredients');
+  // handle  ingredients Prototype
+  const ingredients = document.createElement('ul')
+  ingredients.classList.add('recipe__ingredients')
 
-const ingredientsList = [
-    '1 litre de limonade',
-    '1 litre de sucre',
-    '1 litre de eau',
-    '1 litre de coco',
-    '1 litre de sucre'
-];
+  el.ingredients.forEach((ingredient) => {
+    const ingredientItem = document.createElement('li')
+    ingredientItem.classList.add('recipe__ingredient')
+    if (ingredient.unit) {
+      ingredientItem.textContent =
+        ingredient.ingredient +
+        ' : ' +
+        ingredient.quantity +
+        ' ' +
+        ingredient.unit
+    } else if (
+      ingredient.ingredient &&
+      ingredient.quantity &&
+      !ingredient.unit
+    ) {
+      ingredientItem.textContent =
+        ingredient.ingredient + ' : ' + ingredient.quantity
+    } else if (
+      ingredient.ingredient &&
+      !ingredient.quantity &&
+      !ingredient.unit
+    ) {
+      ingredientItem.textContent = ingredient.ingredient
+    }
+    ingredients.appendChild(ingredientItem)
+  })
 
-for (let i = 0; i < ingredientsList.length; i++) {
-    const ingredient = document.createElement('li');
-    ingredient.classList.add('recipe__ingredient');
-    ingredient.textContent = ingredientsList[i];
-    ingredients.appendChild(ingredient);
-}
+  titleIngredientsContainer.appendChild(ingredients)
 
-titleIngredientsContainer.appendChild(ingredients);
+  //  handle  time Prototype
 
-//  handle  time Prototype
+  const time = document.createElement('div')
+  time.classList.add('recipe__time')
+  time.innerHTML =
+    "<img class='time__image' src='./assets/icons/clockIcon.png'></img>" +
+    `<p class='time-duration'>${el.time} min</p>`
+  timeAndInstructionsContainer.appendChild(time)
 
-const time = document.createElement('div');
-time.classList.add('recipe__time');
-time.innerHTML = "<img class='time__image' src='./assets/icons/clockIcon.png'></img>" + "<p class='time-duration'>30 min</p>";
-timeAndInstructionsContainer.appendChild(time);
+  //  handle  instructions Prototype
+  const instructions = document.createElement('div')
+  instructions.classList.add('recipe__instructions')
 
-//  handle  instructions Prototype
-const instructions = document.createElement('div');
-instructions.classList.add('recipe__instructions');
+  const instructionContent = el.description
 
-const instructionContent =" Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quam ipsum ut ex ab eius aspernatur ratione. Quod, quo quibusdam!"
+  const instruction = document.createElement('p')
+  instruction.classList.add('recipe__instruction')
+  instruction.textContent = instructionContent
+  instructions.appendChild(instruction)
 
-    const instruction = document.createElement('p');
-    instruction.classList.add('recipe__instruction');
-    instruction.textContent = instructionContent;
-    instructions.appendChild(instruction);
+  timeAndInstructionsContainer.appendChild(instructions)
 
+  document.querySelector('#recipes').appendChild(recipe)
+  actualRecipe.push(recipe)
+})
 
-timeAndInstructionsContainer.appendChild(instructions);
-
-document.querySelector('#recipes').appendChild(recipe);
-
-
-});
-
+const searchBar = document.querySelector('#search__input')
+searchBar.addEventListener('input', (e) => {
+  const searchValue = e.target.value
+  console.log(e.target.value)
+  console.log(actualRecipe)
+  actualRecipe.forEach((el) => {
+    if (el.textContent.toLowerCase().includes(searchValue.toLowerCase())) {
+      el.style.display = 'block'
+    } else {
+      el.style.display = 'none'
+    }
+  })
+})
