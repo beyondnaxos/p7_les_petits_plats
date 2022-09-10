8d17cd2a759f5391484f38e6cb9790c5bfbbd0ee
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2022-09-10 09:08:16 +0200

    Update searchEngine.js

--- utils/searchEngine.js
+++ utils/searchEngine.js
@@ -25,44 +25,44 @@ export const searchIngredients = (datasProxy, datas, recipes) => {
 }
 
 
-export const searchAppliance = (datasProxy, datas, recipes) => {
-  document
-    .querySelector('#search__input-appliance')
-    .addEventListener('input', (e) => {
-      const str = e.target.value
-      console.log(datas);
-      if (str.length >= 3 && str.length > datas.searchLength) {
-        const filter = datas.appliances.filter(elt => elt.toLowerCase().includes(str.toLowerCase()));
-        datasProxy.recipes = [...filter]
-      } else if (str == '') {
-        datasProxy.recipes = [...recipes]
-        if(datasProxy.mainSearch){
-          const filter = datas.recipes.filter((elt) =>
-          elt.name.toLowerCase().includes(datasProxy.mainSearch.toLowerCase())
-        )
-        datasProxy.recipes = [...filter]
-        }
-      }
-      datasProxy.searchLength = str.length
-    })
-}
-
-// export const searchAppliance = (datasProxy, datas) => {
+// export const searchAppliance = (datasProxy, datas, recipes) => {
 //   document
 //     .querySelector('#search__input-appliance')
 //     .addEventListener('input', (e) => {
 //       const str = e.target.value
-//       if(str.length > datas.searchLength){
-//       const filter = datas.appliances.filter(elt => elt.toLowerCase().includes(str.toLowerCase()));
-//       console.log('fiiiiiiiilter', filter);
-//       datasProxy.appliances = [...filter]
-     
-//       } else {
-//         datasProxy.recipes = [...Array.from(new Set(datasProxy.recipes.map(elt => elt.appliance)))]
+//       console.log(datas);
+//       if (str.length >= 3 && str.length > datas.searchLength) {
+//         const filter = datas.appliances.filter(elt => elt.toLowerCase().includes(str.toLowerCase()));
+//         datasProxy.recipes = [...filter]
+//       } else if (str == '') {
+//         datasProxy.recipes = [...recipes]
+//         if(datasProxy.mainSearch){
+//           const filter = datas.recipes.filter((elt) =>
+//           elt.name.toLowerCase().includes(datasProxy.mainSearch.toLowerCase())
+//         )
+//         datasProxy.recipes = [...filter]
+//         }
 //       }
 //       datasProxy.searchLength = str.length
 //     })
 // }
+
+export const searchAppliance = (datasProxy, datas) => {
+  document
+    .querySelector('#search__input-appliance')
+    .addEventListener('input', (e) => {
+      const str = e.target.value
+      if(str.length > datas.searchLength){
+      const filter = datas.appliances.filter(elt => elt.toLowerCase().includes(str.toLowerCase()));
+      console.log('fiiiiiiiilter', filter);
+      datasProxy.appliances = [...filter]
+     
+      } else {
+        datasProxy.recipes = [...Array.from(new Set(datasProxy.recipes.map(elt => elt.appliance)))]
+      }
+      datasProxy.searchLength = str.length
+    })
+}
 
 
 
